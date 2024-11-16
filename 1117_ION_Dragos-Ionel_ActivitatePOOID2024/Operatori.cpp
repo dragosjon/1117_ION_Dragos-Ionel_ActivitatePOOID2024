2b820ddc9b8c816ab3a0fb1fb7b79e47f3e3ee5c
dragosjon <107347132+dragosjon@users.noreply.github.com>
2024-11-16 13:30:24 +0200

    Tema + Operatori

---
+++ 1117_ION_Dragos-Ionel_ActivitatePOOID2024/Operatori.cpp
@@ -0,0 +1,161 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Autobuz {
+private:
+	static int nrAutobuze;
+	const int idAutobuz;
+	int capacitate;
+	int nrPersoaneImbarcate;
+	char* producator;
+public:
+
+	//constructor default
+	Autobuz() : idAutobuz(++nrAutobuze) {
+		this->capacitate = 0;
+		this->nrPersoaneImbarcate = 0;
+		this->producator = new char[strlen("Necunoscut") + 1];
+		strcpy(this->producator, "Necunosctu");
+	}
+	//constructor cu toti parametrii
+	Autobuz(int capacitate, int nrPersoaneImbarcate, const char* producator) : idAutobuz(++nrAutobuze) {
+		this->capacitate = capacitate;
+		this->nrPersoaneImbarcate = nrPersoaneImbarcate;
+		this->producator = new char[strlen(producator) + 1];
+		strcpy(this->producator, producator);
+	}
+	
+
+	//operatorul=
+	Autobuz operator=(const Autobuz& a) {
+		if (this != &a)
+		{
+			this->capacitate = a.capacitate;
+			this->nrPersoaneImbarcate = a.nrPersoaneImbarcate;
+			if (this->producator != nullptr) {
+				delete[]this->producator;
+			}
+			this->producator = new char[strlen(a.producator) + 1];
+			strcpy(this->producator, a.producator);
+		}
+		return *this;
+	}
+
+	//metoda getNumarLocuriLibere()
+
+	int getNumarLocuriLibere() {
+		return capacitate - nrPersoaneImbarcate;
+	}
+	//afisare
+	void afisare() {
+		cout << "Id: " << this->idAutobuz << "; ";
+		cout << "Capacitate: " << this->capacitate << "; ";
+		cout << "NrPersoaneImbarcate :";
+		if (nrPersoaneImbarcate > capacitate)
+			cout << "Numarul de persoane a depasit capacitatea autobuzului" << "; ";
+		cout << this->nrPersoaneImbarcate << ";";
+		cout << "Producator: " << this->producator << "; " << endl;
+	}
+
+	//getteri si setteri
+	int getCapacitate() {
+		return this->capacitate;
+	}
+
+	void setCapacitate(int capacitate) {
+		this->capacitate = capacitate;
+	}
+
+
+	int getNrPersoaneImbarcate() {
+		return this->nrPersoaneImbarcate;
+	}
+
+	void setNrPersoaneImbarcate(int nrPersoaneImbarcate) {
+		this->nrPersoaneImbarcate = nrPersoaneImbarcate;
+	}
+	
+
+	///////////////////////////////////////////////////////////////////////    OPERATORI  + METODE     //////////////////////////////////////////////////////////////////////////
+
+
+	//Să se supraîncarce operator de cast la int care determină numărul de persoane urcate deja în autobuz. 
+	operator int() const {
+		return nrPersoaneImbarcate;
+	}
+
+	//Să se supraîncarce operator> care va compara două autobuze după capacitatea acestora ( OPERATOR DE COMPARATIE )
+
+	bool operator>(const Autobuz& a) {
+		return this->capacitate > a.capacitate;
+	}
+
+	//sa se supraincarce  operatorul () functie care determina numarul de persoane care mai pot fi imbarcate pana la atingerea capacitatii maxime a autobuzului
+
+	int operator()() {
+		return capacitate - nrPersoaneImbarcate;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//destructor
+	~Autobuz() {
+		if (this->producator != nullptr)
+			delete[]this->producator;
+	}
+};
+
+int Autobuz::nrAutobuze = 0;
+
+
+int main() {
+	Autobuz autobuz1;
+	autobuz1.afisare();
+
+	Autobuz autobuz2(10, 5, "Mercedes");
+	autobuz2.afisare();
+
+	Autobuz autobuz3(10, 12, "Orio");
+	autobuz3.afisare();
+
+
+	//testare numar locuri libere
+	cout << "Numar locuri libere: " << autobuz3.getNumarLocuriLibere() << "; " << endl;
+
+	//testare operator de cast la int
+	int nrPersoaneImbarcate = autobuz3;
+	cout << "Numarul de persoane imbarcate este: " << nrPersoaneImbarcate << "; " << endl;
+
+	//testare supraincarcarea operatorului >
+	//testare capacitate mai mica
+	autobuz3.setCapacitate(5);
+	if (autobuz3 > autobuz2) {
+		cout << "Autobuzul: 3 are capacitatea mai mare decat autobuzul: 2" << endl;
+	}
+	else {
+		cout << "Autobuzul: 3 are capacitatea mai mica decat autobuzul: 2" << endl;
+	}
+
+	//testare Supraincarcarea operatorului ()
+	cout << "Numarul de persoane care mai pot fi imbarcate in autobuz este: " << autobuz2() << endl;
+
+
+
+}
